2a8339ddbb6fd8c6334a63ddf52793f860fd6d50
benh777 <hamza.kim83@hotmail.fr>
2017-10-21 18:02:35 +0200

    Add files via upload

---
+++ pyphishtank.py
@@ -0,0 +1,13 @@
+from pyphishtank import PhishTank
+
+p = PhishTank()
+
+result = p.check("http://example.com")
+
+if result.in_database:
+  if result.valid:
+    print("{url} is a phish!".format(url=result.url))
+  else:
+    print("{url} is not a phish!".format(url=result.url))
+else:
+  print("{url} is not in the PhishTank database".format(url=result.url))
